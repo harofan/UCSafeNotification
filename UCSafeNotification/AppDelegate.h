a88da14d6835a84c65a05566ecb4befb18fa2171
RPGLiker <fanyang_32012@outlook.com>
2018-07-26 10:42:44 +0800

    提交项目

---
+++ UCSafeNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UCSafeNotification
+//
+//  Created by 范杨 on 2018/7/26.
+//  Copyright © 2018年 RPGLiker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UCSafeNotification/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UCSafeNotification
+//
+//  Created by 范杨 on 2018/7/26.
+//  Copyright © 2018年 RPGLiker. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
